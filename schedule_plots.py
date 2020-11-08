68dc68769e4e0eb05a87ed15bbb9645074b0d52b
chjort <christoffer.hjort1995@gmail.com>
2020-11-08 02:48:26 +0100

    Experiment with hyperparameters

--- schedule_plots.py
+++ schedule_plots.py
@@ -1,7 +1,35 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import tensorflow as tf
 
 EPOCHS = 100
+
+
+# %% PLOT LOGS
+def read_file_logs(file):
+    with open(file) as f:
+        col_names = f.readline().strip("\n").split(",")
+    data = np.genfromtxt(file, delimiter=",", skip_header=1)
+    logs = {col_name: values for col_name, values in zip(col_names, np.transpose(data))}
+    return logs
+
+
+log_files = [
+    # "outputs/vitos_drop01/log.csv",
+    # "outputs/vitos_b2-98_drop01/log.csv",
+    # "outputs/vitos_b2-98_drop01_b1024/log.csv",
+    # "outputs/vitos_b2-98_drop01_b1024_sch/log.csv",
+    "outputs/vitos_b2-98_drop01_b1024_e200/log.csv"
+    "outputs/vitos_b2-98_drop01_b1024_e200_seed/log.csv"
+]
+metrics = ["val_loss", "val_accuracy"]
+fig, axes = plt.subplots(len(metrics), 1)
+for i, metric in enumerate(metrics):
+    for j, file in enumerate(log_files):
+        logs = read_file_logs(file)
+        axes[i].plot(logs[metric], label="file_{}: {}".format(j, metric))
+        axes[i].legend()
+plt.show()
 
 # %% PLOT SCHEDULES
 LR = 0.00006
--- train_driver_os_rec.py
+++ train_driver_os_rec.py
@@ -1,3 +1,7 @@
+from chambers.utils import set_random_seed
+
+set_random_seed(42)
+
 import glob
 import os
 
@@ -7,17 +11,7 @@ from einops import rearrange
 
 from chambers.data.loader import InterleaveTFRecordOneshotDataset
 from chambers.models.transformer import VisionTransformerOS
-
-train_path = "/home/crr/datasets/omniglot/train_records"
-test_path = "/home/crr/datasets/omniglot/test_records"
-
-train_records = glob.glob(os.path.join(train_path, "*.tfrecord"))
-test_records = glob.glob(os.path.join(test_path, "*.tfrecord"))
-
-strategy = tf.distribute.MirroredStrategy()
-
-
-# strategy = tf.distribute.OneDeviceStrategy("/gpu:0")
+from chambers.utils import get_model_memory_usage
 
 
 def flatten_batch(x, y):
@@ -37,29 +31,41 @@ def preprocess(x, y):
     return (x1, x2), y
 
 
-n = 2  # increasing this value causes overfitting
-n_pairs = 128 * strategy.num_replicas_in_sync  # scale batch size with this.
+strategy = tf.distribute.MirroredStrategy()
+# strategy = tf.distribute.OneDeviceStrategy("/gpu:0")
+
+# data parameters
+TRAIN_PATH = "/home/crr/datasets/omniglot/train_records"
+TEST_PATH = "/home/crr/datasets/omniglot/test_records"
+
+# loading data
+train_records = glob.glob(os.path.join(TRAIN_PATH, "*.tfrecord"))
+test_records = glob.glob(os.path.join(TEST_PATH, "*.tfrecord"))
+
+N_PER_PAIR = 2  # increasing this value causes overfitting
+N_PAIRS_PER_DEVICE = 256
+N_PAIRS = N_PAIRS_PER_DEVICE * strategy.num_replicas_in_sync  # scale batch size with this.
 train_dataset = InterleaveTFRecordOneshotDataset(records=train_records,
-                                                 n=n,
+                                                 n=N_PER_PAIR,
                                                  sample_n_random=True,
                                                  shuffle=True,
                                                  reshuffle_iteration=True,
                                                  repeats=-1,
                                                  seed=42)
 train_dataset.map(preprocess)
-train_dataset.batch(n_pairs)
+train_dataset.batch(N_PAIRS)
 train_dataset.map(flatten_batch)
 train_dataset.prefetch(-1)
 
 test_dataset = InterleaveTFRecordOneshotDataset(records=test_records,
-                                                n=n,
+                                                n=N_PER_PAIR,
                                                 sample_n_random=False,
                                                 shuffle=False,
                                                 reshuffle_iteration=False,
                                                 repeats=None,
                                                 seed=42)
 test_dataset.map(preprocess)
-test_dataset.batch(n_pairs)
+test_dataset.batch(N_PAIRS)
 test_dataset.map(flatten_batch)
 test_dataset.prefetch(-1)
 
@@ -67,6 +73,10 @@ train_dataset = train_dataset.dataset
 test_dataset = test_dataset.dataset
 
 # %%
+EPOCHS = 200
+STEPS_PER_EPOCH = 200 // strategy.num_replicas_in_sync
+LR = 0.00006
+
 INPUT_SHAPE = (84, 84, 1)
 PATCH_SIZE = 12
 PATCH_DIM = 128
@@ -84,16 +94,15 @@ with strategy.scope():
                                 dropout_rate=0.1
                                 )
 
-    # lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=1e-4,
-    #                                                              decay_steps=100,
-    #                                                              decay_rate=0.1,
-    #                                                              staircase=True)
+    # LR = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=LR,
+    #                                                     decay_steps=int(0.66 * EPOCHS) * STEPS_PER_EPOCH,
+    #                                                     decay_rate=0.1,
+    #                                                     staircase=True)
 
-    LR = 0.00006
-    optimizer = tfa.optimizers.AdamW(lr=LR,
+    optimizer = tfa.optimizers.AdamW(learning_rate=LR,
                                      weight_decay=1e-4,
                                      beta_1=0.9,
-                                     beta_2=0.999,  # 0.98
+                                     beta_2=0.98,  # 0.999
                                      epsilon=1e-8,
                                      amsgrad=False)
     model.compile(optimizer=optimizer,
@@ -102,15 +111,16 @@ with strategy.scope():
 
 model.summary()
 
-# %%
-EPOCHS = 100
-steps_per_epoch = 200 // strategy.num_replicas_in_sync
+batch_size = 2 * N_PER_PAIR * N_PAIRS_PER_DEVICE
+batch_mem_gb = get_model_memory_usage(batch_size, model)
+print("Batch size of {} in memory: {}GB".format(batch_size, batch_mem_gb))
 
-output_dir = "outputs/vitos_no_tricks_adamw_drop01"
+# %%
+output_dir = "outputs/vitos_b2-98_drop01_b1024_e200_seed"
 os.makedirs(output_dir, exist_ok=True)
 hist = model.fit(train_dataset,
                  epochs=EPOCHS,
-                 steps_per_epoch=steps_per_epoch,
+                 steps_per_epoch=STEPS_PER_EPOCH,
                  validation_data=test_dataset,
                  callbacks=[
                      tf.keras.callbacks.CSVLogger(os.path.join(output_dir, "log.csv")),
